bf2fb661b9314a3bfcad6766dfea1b96fe88b087
wleskone <108864374+wleskone@users.noreply.github.com>
2023-11-24 15:33:06 +0300

    Сделал (#18)
    Co-authored-by: ithubspb <150043264+ithubspb@users.noreply.github.com>

--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { Headline3 } from "../lib/main";
 import { Numeric3 } from "../lib/main";
-
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <Layout>
   <React.StrictMode>
+    <Layout>
     <h1>Sandbox</h1>
-    <Numeric3 text="Проверка"/>
+    </Layout>
   </React.StrictMode>
-  </Layout>
 );
