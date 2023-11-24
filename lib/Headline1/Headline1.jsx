ffdd4dcf16e02c801097abdf82ac772760acfbe1
Y.Karpov <KarpovYAA23@spb.ithub.ru>
2023-11-24 14:53:22 +0300

    implemented

---
+++ lib/Headline1/Headline1.jsx
@@ -0,0 +1,6 @@
+import './Headline1.css'
+
+export function Heading1({ text }) {
+    return <h1 className="Heading1">{text}</h1>;
+}
+
--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Heading1 } from "../lib/Headline1/Headline1";
 
 import { Button } from "../lib/main";
 import { Heading2 } from "../lib/main";
@@ -10,7 +11,6 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <h1>Sandbox</h1>
-    <Heading2 text="Zagolovok" />
-    <Button text="Отправить" />
+    <Heading1 text="Тестовый текст" />
   </React.StrictMode>
 );
