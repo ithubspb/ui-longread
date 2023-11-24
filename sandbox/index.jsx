d1629c4ca358f1538f24f06347784c07954ea6b7
Valdemar697489 <vladimir.efimychev@gmail.com>
2023-11-24 18:35:51 +0300

    fix

--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Heading1 } from "../lib/Headline1/Headline1";
-
 import { Numeric2 } from "../lib/main";
 import { Headline3 } from "../lib/main";
 import { Numeric3 } from "../lib/main";
+import { Layout } from "../lib/main";
+
+import './style.css'
+
+import moon from './moon.png'
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -12,9 +16,22 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Layout>
-      <h1>Sandbox</h1>
-      <Numeric2 text="TEST" />
-      <Heading1 text="Тестовый текст" />
+      <div className="row">
+        <img  className = 'Kartinka' src={moon} alt="" />
+        <div className = 'Nachalo'>Научный лонгрид</div>
+        <p className = 'Zagolovok50'> Движение <br /> Ракет и <br /> спутников</p>
+      </div>
+      <p className = 'Zagolovok'>Почему люди не летают<br />как птицы ?</p>
+      
+      <Numeric2   text="Сила притяжения, действующая на человека, гораздо больше других сил действующих на него, например, силы Архимеда. Орёл же может парить над горами, потому что сила давления восходящих потоков воздуха уравновешивает силу тяжести действующую на него.
+
+Человек для полётов приспособлен точно также как обыкновенный булыжник. А может ли булыжник летать? Для того, чтобы булыжник оторвался от поверхности Земли и хоть краткое время находился в состоянии полёта, его необходимо разогнать до какой‑либо скорости."/>
+      <Heading1 text= "" />
+      
     </Layout>
   </React.StrictMode>
 );
+
+
+
+
