18201be4090b86a74cb2f21ed33a06c462c5669c
Daimos2 <151909578+Daimos2@users.noreply.github.com>
2023-11-24 15:40:37 +0300

    9 numeric2 text (#20)
    * задезайнил кнопку
    
    * Сделал
    
    ---------
    
    Co-authored-by: ithubspb <150043264+ithubspb@users.noreply.github.com>

--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Heading1 } from "../lib/Headline1/Headline1";
 
+import { Numeric2 } from "../lib/main";
 import { Headline3 } from "../lib/main";
 import { Numeric3 } from "../lib/main";
 
@@ -11,8 +12,9 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Layout>
-    <h1>Sandbox</h1>
-    <Heading1 text="Тестовый текст" />
+      <h1>Sandbox</h1>
+      <Numeric2 text="TEST" />
+      <Heading1 text="Тестовый текст" />
     </Layout>
   </React.StrictMode>
 );
