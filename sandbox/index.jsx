686c2a9664423c7ba1cb2a13db31b8fa9af4eeab
ithubspb <150043264+ithubspb@users.noreply.github.com>
2023-11-24 12:22:14 +0300

    Задизайнил кнопку (#4)
    * Задизайнил кнопку
    
    * Пофиксил пэдинг
    
    ---------
    
    Co-authored-by: Lex Trofimov <daslef93@gmail.com>

--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { Button } from "../lib/main";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
     <h1>Sandbox</h1>
+    <Button text="Отправить" />
   </React.StrictMode>
 );
