11528e50ac6e68b31310c1e96bb573422ca1ab82
Eseniia <moiseevaesenia@gmail.com>
2023-11-24 14:15:26 +0300

    Done

---
+++ lib/Heading2/Heading2.jsx
@@ -0,0 +1,5 @@
+import './Heading2.css'
+
+export function Heading2({ text }) {
+    return <h2 className='H2'>{text}</h2>
+}
--- sandbox/index.jsx
+++ sandbox/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import { Button } from "../lib/main";
+import { Heading2 } from "../lib/main";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,6 +10,7 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <h1>Sandbox</h1>
+    <Heading2 text="Zagolovok" />
     <Button text="Отправить" />
   </React.StrictMode>
 );
